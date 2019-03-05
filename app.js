fab4dc9796a891102dbfb3cfae4773927eaa25ab
Ezekiel blanc <ezekielblanc1@gmail.com>
2019-03-05 23:30:36 +0100

    handled auth

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const passport = require('passport');
+const auth = require('./routes/auth');
 const app = express();
+//Passport config
+
+require('./config/passport')(passport)
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) =>{
   res.send('it works');
-})
+});
+
+app.use('/auth', auth)
 
 app.listen(port, () => {
   console.log(`app running on port ${port}`)
