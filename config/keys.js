23c0d792a639855aa303ed8e725dddb60c09700f
Ezekiel blanc <ezekielblanc1@gmail.com>
2019-03-06 09:37:25 +0100

    keys added to heroku

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 'mongodb://Dan:abc123@ds137703.mlab.com:37703/storybook-dev',
+  googleClientID: '808370811757-c17gngrbsefm2o8d94mtcd4n06tqhh0i.apps.googleusercontent.com',
+  googleClientSecret: 'XvGmOplg2cnIPHvu6WIYb0vz'
+}
